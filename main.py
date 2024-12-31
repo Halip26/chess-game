0e4d4ead778851291759dbaa3461b3c5a75c420d
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-12-31 14:57:09 +0800

    refactor image loading to use local asset paths instead of remote URLs

--- main.py
+++ main.py
@@ -105,81 +105,55 @@ turn_step = 0
 selection = 100
 valid_moves = []
 
-# url for chess pieces images
-image_urls = [
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025946/black_queen.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025948/black_king.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025345/black_rook.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025951/black_bishop.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025947/black_knight.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025945/black_pawn.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025952/white_queen.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025943/white_king.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025949/white_rook.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025944/white_bishop.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025325/white_knight.png",
-    "https://media.geeksforgeeks.org/wp-content/uploads/20240302025953/white_pawn.png",
+local_paths = [
+    "assets/black_queen.png",
+    "assets/black_king.png",
+    "assets/black_rook.png",
+    "assets/black_bishop.png",
+    "assets/black_knight.png",
+    "assets/black_pawn.png",
+    "assets/white_queen.png",
+    "assets/white_king.png",
+    "assets/white_rook.png",
+    "assets/white_bishop.png",
+    "assets/white_knight.png",
+    "assets/white_pawn.png",
 ]
 
-# load in game piece images (queen, king, rook, bishop, knight, pawn) x 2
-black_queen = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[0]).content))
-)
+black_queen = pygame.image.load(local_paths[0])
 black_queen = pygame.transform.scale(black_queen, (80, 80))
 black_queen_small = pygame.transform.scale(black_queen, (45, 45))
-black_king = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[1]).content))
-)
+black_king = pygame.image.load(local_paths[1])
 black_king = pygame.transform.scale(black_king, (80, 80))
 black_king_small = pygame.transform.scale(black_king, (45, 45))
-black_rook = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[2]).content))
-)
+black_rook = pygame.image.load(local_paths[2])
 black_rook = pygame.transform.scale(black_rook, (80, 80))
 black_rook_small = pygame.transform.scale(black_rook, (45, 45))
-black_bishop = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[3]).content))
-)
+black_bishop = pygame.image.load(local_paths[3])
 black_bishop = pygame.transform.scale(black_bishop, (80, 80))
 black_bishop_small = pygame.transform.scale(black_bishop, (45, 45))
-black_knight = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[4]).content))
-)
+black_knight = pygame.image.load(local_paths[4])
 black_knight = pygame.transform.scale(black_knight, (80, 80))
 black_knight_small = pygame.transform.scale(black_knight, (45, 45))
-black_pawn = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[5]).content))
-)
+black_pawn = pygame.image.load(local_paths[5])
 black_pawn = pygame.transform.scale(black_pawn, (65, 65))
 black_pawn_small = pygame.transform.scale(black_pawn, (45, 45))
-white_queen = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[6]).content))
-)
+white_queen = pygame.image.load(local_paths[6])
 white_queen = pygame.transform.scale(white_queen, (80, 80))
 white_queen_small = pygame.transform.scale(white_queen, (45, 45))
-white_king = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[7]).content))
-)
+white_king = pygame.image.load(local_paths[7])
 white_king = pygame.transform.scale(white_king, (80, 80))
 white_king_small = pygame.transform.scale(white_king, (45, 45))
-white_rook = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[8]).content))
-)
+white_rook = pygame.image.load(local_paths[8])
 white_rook = pygame.transform.scale(white_rook, (80, 80))
 white_rook_small = pygame.transform.scale(white_rook, (45, 45))
-white_bishop = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[9]).content))
-)
+white_bishop = pygame.image.load(local_paths[9])
 white_bishop = pygame.transform.scale(white_bishop, (80, 80))
 white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
-white_knight = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[10]).content))
-)
+white_knight = pygame.image.load(local_paths[10])
 white_knight = pygame.transform.scale(white_knight, (80, 80))
 white_knight_small = pygame.transform.scale(white_knight, (45, 45))
-white_pawn = pygame.image.load(
-    BytesIO(rembg.remove(requests.get(image_urls[11]).content))
-)
+white_pawn = pygame.image.load(local_paths[11])
 white_pawn = pygame.transform.scale(white_pawn, (65, 65))
 white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
 
